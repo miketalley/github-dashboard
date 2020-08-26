964ebf26666aa7e1adc562c26f45593e4f0d985c
Mike Talley <miketalley@hotmail.com>
2020-08-25 23:21:03 -0400

    updated serverless function with 302 redirect

--- api/github-auth.js
+++ api/github-auth.js
@@ -8,8 +8,12 @@ const ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 
 module.exports = (req, res) => {
   const { code } = req.query;
-  const QUERY_STRING = `?code=${code}&client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&state=${GITHUB_STATE}&redirect_uri=${GITHUB_REDIRECT_URI}`;
+  const queryString = `?code=${code}&client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&state=${GITHUB_STATE}&redirect_uri=${GITHUB_REDIRECT_URI}`;
+  const redirectUrl = `${ACCESS_TOKEN_URL}${queryString}`;
 
-  res.set(`location`, `${ACCESS_TOKEN_URL}${QUERY_STRING}`);
-  res.status(301).send();
+  res.writeHead(302, {
+    Location: redirectUrl,
+  });
+
+  res.end();
 };
